0b470dbe0a399a996c924a5fb0adec0d9838d5d8
Laura Hall <podinjshmi1993@gmail.com>
2023-09-05 13:31:39 +0100

    fetching dog img from API, basic app layout

--- src/components/VotePicker.tsx
+++ src/components/VotePicker.tsx
@@ -1,3 +1,57 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+interface APIResponse {
+    message: string;
+    status: string;
+}
+
+interface DogImgSrc {
+    dogOne: string;
+    dogTwo: string;
+}
+
 export default function VotePicker(): JSX.Element {
-    return <h2> Random Dog Breeds pics </h2>;
+    const [dogsImageSrc, setDogsImageSrc] = useState<DogImgSrc | null>(null);
+
+    useEffect(() => {
+        createImgData();
+    }, []);
+
+    async function createImgData() {
+        try {
+            const dogOneValue = await fetchDogImg();
+            const dogTwoValue = await fetchDogImg();
+            const imageSrcData: DogImgSrc = {
+                dogOne: dogOneValue,
+                dogTwo: dogTwoValue,
+            };
+            setDogsImageSrc(imageSrcData);
+        } catch (error) {
+            console.log("Error fetching data", error);
+        }
+    }
+
+    async function fetchDogImg() {
+        const APIUrl = "https://dog.ceo/api/breeds/image/random";
+        const response = await axios.get(APIUrl);
+        const result: APIResponse = response.data;
+        return result.message;
+    }
+
+    return (
+        <div className="vote-img-container">
+            <img
+                className="dog-img"
+                src={dogsImageSrc?.dogOne}
+                alt="random breed"
+            />
+            <br />
+            <img
+                className="dog-img"
+                src={dogsImageSrc?.dogTwo}
+                alt="random breed"
+            />
+        </div>
+    );
 }
